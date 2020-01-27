077b651e5733716e54f10d7c77f35cdb84068500
deisylh <deisylh@gmail.com>
2020-01-26 20:56:48 -0500

    actualiza el proyecto

---
+++ src/main/java/com/RJ45/demo/MsPersonaSdApplication.java
@@ -0,0 +1,13 @@
+package com.RJ45.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPersonaSdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPersonaSdApplication.class, args);
+	}
+
+}

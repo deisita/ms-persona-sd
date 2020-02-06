2748a5a8c43d9c38777b40ddbff62059a73106c3
deisita <deisylh@gmail.com>
2020-02-06 00:28:44 -0500

    ms local

--- script/ddl.sql
+++ script/ddl.sql
@@ -1,12 +1,12 @@
-create schema postgres/deisy;
-
-create table postgres/deisy.persona
+create schema deisy;
+create schema authorization postgres;
+create table deisy.persona
 (
    id              decimal(10),
    primer_nombre   varchar(255),
    segundo_nombre  varchar(255),
-   primer_apellido varchar(255)
-   segundo_apellido varchar(255)
+   primer_apellido varchar(255),
+   segundo_apellido varchar(255),
    fecha_nacimiento date,
    genero           varchar(255)
 

8ac851aa82b48922f599cc3af8a352e78023ffa7
deisita <deisy_720@hotmail.com>
2020-02-06 23:33:31 -0500

    Se mapeo el objeto y se realizaron controles de errores.

--- src/main/java/com/RJ45/demo/controllers/PersonaController.java
+++ src/main/java/com/RJ45/demo/controllers/PersonaController.java
@@ -3,28 +3,22 @@ package com.RJ45.demo.controllers;
 import com.RJ45.demo.entities.PersonaEntity;
 import com.RJ45.demo.repositories.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 
 @RestController
+@RequestMapping("/personas")
 public class PersonaController{
 
     @Autowired
     private PersonaRepository personaRepository;
 
-
     @GetMapping("/")
-    public String getInicio()
-    {
-        return "Hola!";
-
-    }
-
-    @GetMapping("/personas")
     public List<PersonaEntity> getPersonas(){
         Iterable<PersonaEntity> result = personaRepository.findAll();
         List<PersonaEntity> personasList = new ArrayList<PersonaEntity>();
@@ -32,11 +26,23 @@ public class PersonaController{
         return personasList;
     }
 
-    @GetMapping("persona")
-    public PersonaEntity getPersonaId(@PathVariable("id") Integer id) {
-        PersonaEntity obj = personaRepository.findById(id).get();
-        return obj;
-        //return id;
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getPersonaId(@PathVariable("id") Integer id) {
+        PersonaEntity obj=null;
+        Map<String, Object> response = new HashMap<>();
+        try{
+            obj = personaRepository.findById(id).orElse(null);
+            if (obj==null){
+                response.put("mensaje","No se pudo encontrar a la persona.");
+                return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(obj, HttpStatus.OK);
+        }
+        catch(DataAccessException e){
+            response.put("mensaje","Error al realizar la consulta en la base de datos.");
+            response.put("Error",e.getMessage()+"##"+e.getMostSpecificCause().getMessage());
+            return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/guardaPersona")
